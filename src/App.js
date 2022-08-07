7ec5f04dc39011aed15beefe4da5461e9caf4537
Devbrat Raghuvanshi <devbrat.raghuvanshi@annalect.com>
2022-08-08 05:08:22 +0530

    facemesh complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
+import { drawMesh } from './utilities';
 
 
 function App() {
@@ -18,7 +19,10 @@ function App() {
     const net = await facemesh.load({
       inputResolution:{width:640, height:480},scale:0.8
     });
-    detect(net);
+    setInterval(() => {
+      detect(net);
+    }, 100);
+    
   }
 
   //  detect function
@@ -29,24 +33,30 @@ function App() {
       webcamRef.current.video.readyState === 4){
         // get v properties, 
         const video = webcamRef.current.video;
-        const videoWidth = video.width;
-        const videoHeight = video.height;
+        const videoWidth = webcamRef.current.video.clientWidth;
+        const videoHeight = webcamRef.current.video.clientHeight;
 
         // set video width, 
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
 
         // set canvas width, 
         canvasRef.current.width = videoWidth;
         canvasRef.current.height = videoHeight;
 
+        
         // make detection ,
         const face = await net.estimateFaces(video);
         console.log(face)
         // get canvas context for  drawing
+        const ctx = canvasRef.current.getContext("2d");
+        requestAnimationFrame(()=>{drawMesh(face, ctx)});
+        
 
       }
 
   }
-  funcFacemesh();
+  useEffect(()=>{funcFacemesh()}, []);
   return (
     <div className="App">
       <header className='App-header'>
@@ -57,7 +67,7 @@ function App() {
             marginLeft: "auto",
             marginRight: "auto",
             textAlign: "centr",
-            zIndex: 9,
+            zIndex: 8,
             width: 640,
             height: 480
           }
@@ -69,7 +79,7 @@ function App() {
             marginLeft: "auto",
             marginRight: "auto",
             textAlign: "centr",
-            zIndex: 9,
+            zIndex: 10,
             width: 640,
             height: 480
           }
